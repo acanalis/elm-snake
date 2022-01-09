7f17c282b3d8cf759ec927af3fd94cd5ce32233b
A. Canalis <agustin.canalis@gmail.com>
2022-01-09 16:54:25 -0300

    Refactor to lists, eliminate advance function

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,5 @@
 module Main exposing (..)
 
-import Array exposing (Array)
 import Browser
 import Browser.Events
 import Html exposing (..)
@@ -12,6 +11,7 @@ import Svg.Attributes exposing (..)
 import Time
 
 
+main : Program () Model Msg
 main =
     Browser.element { init = init, view = view, update = update, subscriptions = subscriptions }
 
@@ -20,38 +20,15 @@ type alias Vec =
     { x : Int, y : Int }
 
 
-zerovec : Vec
-zerovec =
-    Vec 0 0
-
-
 vecsum : Vec -> Vec -> Vec
 vecsum a b =
     Vec (a.x + b.x) (a.y + b.y)
 
 
-vecdiff : Vec -> Vec -> Vec
-vecdiff a b =
-    Vec (a.x - b.x) (a.y - b.y)
-
-
-vecmul : Int -> Vec -> Vec
-vecmul p a =
-    Vec (a.x * p) (a.y * p)
-
-
-norm : Vec -> Vec
-norm a =
-    if a.x == 0 then
-        Vec 0 (a.y // abs a.y)
-
-    else
-        Vec (a.x // abs a.x) 0
-
-
 type Msg
-    = Turn (Maybe Vec)
-    | Step
+    = Nop
+    | ToggleGamestate
+    | Move Vec
     | UpdateFood Vec
 
 
@@ -64,18 +41,21 @@ type GameState
 type alias Model =
     { gamestate : GameState
     , snake : List Vec
+    , direction : Vec
     , food : Vec
     }
 
 
+field : Vec
 field =
-    Vec 40 20
+    Vec 30 20
 
 
 init : () -> ( Model, Cmd Msg )
 init () =
     ( { gamestate = Play
-      , snake = [ Vec (field.x // 2) (field.y // 2), Vec (field.x // 2 - 4) (field.y // 2) ]
+      , snake = List.map (\i -> Vec (field.x // 2 - i) (field.y // 2)) (List.range 0 4)
+      , direction = Vec 1 0
       , food = Vec 6 6
       }
     , Cmd.none
@@ -84,9 +64,9 @@ init () =
 
 view : Model -> Html Msg
 view model =
-    div [ style "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);"]
-        [ p [] [text (viewgamestate model.gamestate)],
-          svg
+    div [ style "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);" ]
+        [ p [] [ text (viewgamestate model.gamestate) ]
+        , svg
             [ width <| String.fromInt <| 10 * field.x
             , height <| String.fromInt <| 10 * field.y
             , style "border: thick solid #000000"
@@ -99,7 +79,7 @@ view model =
                 ]
                 []
             , Svg.circle [ cx (10 * model.food.x |> String.fromInt), cy (10 * (field.y - model.food.y) |> String.fromInt), r "4" ] []
-            ] 
+            ]
         ]
 
 
@@ -107,13 +87,13 @@ viewgamestate : GameState -> String
 viewgamestate gs =
     case gs of
         Play ->
-            "Play with the Arrow keys"
+            "Arrows to Play, space to Pause."
 
         Lose ->
-            "You lost! Press any key to start over"
+            "You lost! Press space to restart"
 
         Pause ->
-            "Pause"
+            "Paused. Press space to continue"
 
 
 viewsnake : List Vec -> String
@@ -124,150 +104,121 @@ viewsnake snake =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update message model =
     case ( model.gamestate, message ) of
+        ( _, Nop ) ->
+            ( model, Cmd.none )
+
+        ( Pause, ToggleGamestate ) ->
+            ( { model | gamestate = Play }, Cmd.none )
+
+        ( Play, ToggleGamestate ) ->
+            ( { model | gamestate = Pause }, Cmd.none )
+
         ( Pause, _ ) ->
             ( model, Cmd.none )
 
-        ( Lose, Turn _ ) ->
+        ( Lose, ToggleGamestate ) ->
             init ()
 
         ( Lose, _ ) ->
             ( model, Cmd.none )
 
-        ( Play, msg ) ->
+        ( Play, UpdateFood newpos ) ->
+            ( { model | food = newpos }, Cmd.none )
+
+        ( Play, Move dir ) ->
             let
-                nextmodel =
-                    advanceplayfun msg model
+                currhead =
+                    List.head model.snake |> withDefault (Vec 0 0)
 
-                nextmodelhead =
-                    List.head nextmodel.snake |> withDefault zerovec
+                nexthead =
+                    vecsum currhead dir
 
-                isoffbounds =
-                    not (0 <= nextmodelhead.x && nextmodelhead.x <= field.x && 0 <= nextmodelhead.y && nextmodelhead.y <= field.y)
+                nextsnake =
+                    if iseatingfood then
+                        nexthead :: model.snake
+
+                    else
+                        nexthead :: List.take (List.length model.snake - 1) model.snake
+
+                iseatingfood =
+                    nexthead == model.food
 
                 iseatingitself =
-                    isonsnake (List.tail nextmodel.snake |> withDefault []) nextmodelhead
+                    List.any ((==) nexthead) model.snake
+
+                isoffbounds =
+                    not (0 <= nexthead.x && nexthead.x <= field.x && 0 <= nexthead.y && nexthead.y <= field.y)
             in
             if isoffbounds || iseatingitself then
-                ( { nextmodel | gamestate = Lose }, Cmd.none )
+                ( { model | snake = nextsnake, direction = dir, gamestate = Lose }, Cmd.none )
 
-            else if isonsnake nextmodel.snake model.food then
-                ( nextmodel, generatefood )
+            else if iseatingfood then
+                ( { model | snake = nextsnake, direction = dir }, generatefood )
 
             else
-                ( nextmodel, Cmd.none )
+                ( { model | snake = nextsnake, direction = dir }, Cmd.none )
 
 
 generatefood : Cmd Msg
 generatefood =
     Random.generate (\( x, y ) -> UpdateFood <| Vec x y) (Random.pair (Random.int 0 field.x) (Random.int 0 field.y))
 
-isonsnake : List Vec -> Vec -> Bool
-isonsnake snake point =
-    case snake of
-        a :: b :: c ->
-            let
-                d =
-                    vecdiff a point
-
-                e =
-                    vecdiff b point
-            in
-            if d.x * e.y - d.y * e.x == 0 && d.x * e.x + d.y * e.y <= 0 then
-                --If the points are colineal, and point is between/equal to them
-                True
-
-            else
-                isonsnake (b :: c) point
-
-        _ ->
-            False
-
-
-advanceplayfun : Msg -> Model -> Model
-advanceplayfun msg model =
-    let
-        head = List.head model.snake |> withDefault zerovec
-
-        headdir =
-            vecdiff head (List.tail model.snake |> Maybe.andThen List.head |> withDefault zerovec) |> norm
-
-        nexthead =
-            vecsum head headdir
-
-        eating =
-            nexthead == model.food
-    in
-    case msg of
-        Turn direction ->
-            case direction of
-                Nothing ->
-                    model
-
-                Just dir ->
-                    if dir.x * headdir.x + dir.y * headdir.y >= 0 then
-                        { model | snake = (vecsum head dir)::model.snake |> steptail eating }
-
-                    else
-                        model
-
-        Step ->
-            { model | snake = nexthead::(List.tail model.snake |> withDefault []) |> steptail eating }
-
-        UpdateFood newpos ->
-            { model | food = newpos }
-
-
-steptail : Bool -> List Vec -> List Vec
-steptail eating snake =
-    let
-        revsnake = List.reverse snake
-        tail =
-            List.head revsnake |> withDefault zerovec
-
-        pretail =
-            List.tail revsnake |> Maybe.andThen List.head |> withDefault zerovec
-
-        taildir =
-            vecdiff pretail tail |> norm
-
-        nexttail =
-            vecsum tail taildir
-    in
-    if eating then
-        snake
-
-    else if pretail == nexttail && List.length revsnake /= 2 then
-        List.reverse (List.tail revsnake|> withDefault [])
-
-    else
-        List.reverse (nexttail::(List.tail revsnake |> withDefault []))
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch
-        [ Time.every 1000 (\_ -> Step)
-        , Browser.Events.onKeyDown keydecoder
+        [ if model.gamestate == Play then
+            Time.every 1000
+                (\_ ->
+                    Move model.direction
+                )
+
+          else
+            Sub.none
+        , Browser.Events.onKeyDown (keydecoder model)
         ]
 
 
-keydecoder : Decode.Decoder Msg
-keydecoder =
+keydecoder : Model -> Decode.Decoder Msg
+keydecoder model =
     Decode.map
         (\s ->
-            case s of
-                "ArrowRight" ->
-                    Turn (Just (Vec 1 0))
+            let
+                newmessage =
+                    case s of
+                        "ArrowRight" ->
+                            Move (Vec 1 0)
 
-                "ArrowLeft" ->
-                    Turn (Just (Vec -1 0))
+                        "ArrowLeft" ->
+                            Move (Vec -1 0)
 
-                "ArrowUp" ->
-                    Turn (Just (Vec 0 1))
+                        "ArrowUp" ->
+                            Move (Vec 0 1)
 
-                "ArrowDown" ->
-                    Turn (Just (Vec 0 -1))
+                        "ArrowDown" ->
+                            Move (Vec 0 -1)
 
-                _ ->
-                    Turn Nothing
+                        " " ->
+                            ToggleGamestate
+
+                        _ ->
+                            Nop
+
+                newd =
+                    case newmessage of
+                        Move dir ->
+                            dir
+
+                        _ ->
+                            model.direction
+
+                d =
+                    model.direction
+            in
+            if d.x * newd.y - d.y * newd.x == 0 && d.x * newd.x + d.y * newd.y <= 0 then
+                Nop
+
+            else
+                newmessage
         )
         (Decode.field "key" Decode.string)
